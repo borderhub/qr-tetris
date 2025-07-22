1254683ef4be2f8fe3d0a28c398a419927723029
“borderhub” <shirabegroup@gmail.com>
2025-07-22 14:53:16 +0900

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   basePath: isProd ? '/qr-tetris' : '',
   assetPrefix: isProd ? '/qr-tetris/' : '',
   images: {
-    unoptimized: isProd, // GitHub Pagesでは最適化を無効化
+    unoptimized: true,
   },
-  ...(isProd && { output: 'export' }), // 本番時のみ静的エクスポートを有効化
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
