0e0c7359ef42c8db3f11d14c1e4059e2a043d5e6
“borderhub” <shirabegroup@gmail.com>
2025-07-22 14:03:39 +0900

    github pages対応

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isProd ? '/qr-tetris' : '',
+  assetPrefix: isProd ? '/qr-tetris/' : '',
+  images: {
+    unoptimized: isProd, // GitHub Pagesでは最適化を無効化
+  },
+  ...(isProd && { output: 'export' }), // 本番時のみ静的エクスポートを有効化
 };
 
 export default nextConfig;
